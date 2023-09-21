fcd49351e883dc6e329668545dddd0393e858d8d
Rafael Urbina <rafaelurbinan@hotmail.com>
2023-09-21 16:16:13 -0400

    Refactoring

--- src/components/features/Features.tsx
+++ src/components/features/Features.tsx
@@ -8,40 +8,25 @@ export type FeatureType = {
 	content: ReactNode	
 }
 
-
-
 const Features = () => {
 	
 	return (
 		<ul className={styles.featuresList}>
-			{features.map(feature => {
-				return (
-					<li key={feature.id} className={styles.featureItem}>
-						<img src={feature.iconSrc} alt='Feature Icon' />	
-						{feature.content}
-					</li>
-				)
-			})}
+			<li className={styles.featureItem}>
+						<img src={blueCheck} alt='Feature Icon' />	
+						<p className={styles.normalFont}>Negative Ion Technology may <span className={styles.strongFont}>help with allergens</span></p>
+			</li>
+			<li className={styles.featureItem}>
+						<img src={blueCheck} alt='Feature Icon' />	
+						<p className={styles.normalFont}>Designed for <span className={styles.strongFont}>air rejuvenation</span></p>
+			</li>
+			<li className={styles.featureItem}>
+						<img src={blueCheck} alt='Feature Icon' />	
+						<p className={styles.normalFont}><span className={styles.strongFont}>Perfect for every room</span>in all types of places</p>
+			</li>			
 		</ul>
 	)
 }
 
 export default Features
 
-const features: FeatureType[] = [
-	{
-		id: 1,
-		iconSrc: blueCheck,
-		content: <p className={styles.normalFont}>Negative Ion Technology may <span className={styles.strongFont}>help with allergens</span></p>
-	},
-	{
-		id: 2,
-		iconSrc: blueCheck,
-		content: <p className={styles.normalFont}>Designed for <span className={styles.strongFont}>air rejuvenation</span></p>
-	},
-	{
-		id: 3,
-		iconSrc: blueCheck,
-		content: <p className={styles.normalFont}><span className={styles.strongFont}>Perfect for every room</span>in all types of places</p>
-	}
-]
